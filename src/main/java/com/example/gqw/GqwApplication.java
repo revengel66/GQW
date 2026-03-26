5b15f7ba244d45a8c9367f22ad870fe004c0a97d
Natalia <natalia200218@yandex.ru>
2026-03-27 03:34:12 +0400

    begin

---
+++ src/main/java/com/example/gqw/GqwApplication.java
@@ -0,0 +1,13 @@
+package com.example.gqw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqwApplication.class, args);
+    }
+
+}
